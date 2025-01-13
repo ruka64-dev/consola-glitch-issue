7411335c1e91494d3ca11c139f837de0cb9c9765
ruka64-dev <192672116+ruka64-dev@users.noreply.github.com>
2025-01-13 17:40:15 +0900

    options list is now generated with Array mapping

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
 import consola from 'consola';
 
 async function main() {
-	await consola.prompt('Choose alphabet', {
+	const options = [...Array(50).keys()].map((i) => ({
+		value: `value-${i}`,
+		label: `label-${i}`,
+	}));
+	await consola.prompt('This is looooooong multiselect list!', {
 		type: 'multiselect',
-		options: [
-			{ value: 'a', label: 'a' },
-			{ value: 'b', label: 'b' },
-			{ value: 'c', label: 'c' },
-			{ value: 'd', label: 'd' },
-			{ value: 'e', label: 'e' },
-			{ value: 'f', label: 'f' },
-			{ value: 'g', label: 'g' },
-			{ value: 'h', label: 'h' },
-			{ value: 'i', label: 'i' },
-			{ value: 'j', label: 'j' },
-			{ value: 'k', label: 'k' },
-			{ value: 'l', label: 'l' },
-			{ value: 'm', label: 'm' },
-			{ value: 'n', label: 'n' },
-			{ value: 'o', label: 'o' },
-			{ value: 'p', label: 'p' },
-			{ value: 'q', label: 'q' },
-			{ value: 'r', label: 'r' },
-			{ value: 's', label: 's' },
-			{ value: 't', label: 't' },
-			{ value: 'u', label: 'u' },
-			{ value: 'v', label: 'v' },
-			{ value: 'w', label: 'w' },
-			{ value: 'x', label: 'x' },
-			{ value: 'y', label: 'y' },
-			{ value: 'z', label: 'z' },
-		],
+		options: options,
 	});
 }
 
